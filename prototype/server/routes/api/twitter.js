568fe9ca8c9d42cd867690355460fe7c739dde2c
yoontsy2000 <yoontsy2000@gmail.com>
2021-05-03 23:47:08 -0400

    Cleaned up code by putting them into their appropriate routes

---
+++ prototype/server/routes/api/twitter.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const Twitter = require('twitter');
+const config = require('dotenv/config');
+
+const API_KEY = process.env.API_KEY;
+const API_SECRET_KEY = process.env.API_SECRET_KEY;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACESSS_SECRET_TOKEN = process.env.ACESSS_SECRET_TOKEN;
+
+const client = new Twitter({
+  consumer_key: API_KEY,
+  consumer_secret: API_SECRET_KEY,
+  access_token_key: ACCESS_TOKEN,
+  access_token_secret: ACESSS_SECRET_TOKEN
+});
+
+const search_tweets = (query) => new Promise((resolve, reject) => {
+  client.get('search/tweets', {q: query, result_type: 'popular', count: 5}, function(error, tweets, response) {
+    if (!error) {
+      resolve(tweets);
+    } else {
+      reject("Returned with error:", error);
+    }
+ });
+})
+
+router.get('/test', (req, res) => {
+  res.send("Hello!");
+})
+
+router.get('/search', (req, res) => {
+  const query = req.query.search
+  console.log(query)
+  search_tweets(query).then(tweets => {
+    res.send(tweets);
+  }).catch(err => console.log("Error:", err))
+});
+
+
+module.exports = router;
---
+++ prototype/server/routes/api/watson.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+const config = require('dotenv/config');
+
+const WATSON_API_KEY = process.env.WATSON_API_KEY;
+const WATSON_API_URL = process.env.WATSON_API_URL;
+
+const SAMPLE_TEXT = {
+  "text": "Team, I know that times are tough! Product sales have been disappointing for the past three quarters. We have a competitive product, but we need to do a better job of selling it!"
+}
+
+const toneAnalyzer = new ToneAnalyzerV3({
+  authenticator: new IamAuthenticator({ apikey: WATSON_API_KEY }),
+  version: '2016-05-19',
+  serviceUrl: WATSON_API_URL
+});
+
+router.get('/tone', (req, res) => {
+  const query = req.query.text
+  toneAnalyzer.tone(
+    {
+      toneInput: query,
+      contentType: 'text/plain'
+    })
+    .then(response => {
+      res.send(JSON.stringify(response.result, null, 2));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+
+
+module.exports = router;
