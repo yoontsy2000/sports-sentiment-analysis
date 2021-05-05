a9a2e49499848902f7a021184601f0f41a96dfaa
Alvaro Carrascosa <varocarras@gmail.com>
2021-05-05 15:04:51 -0400

    Main League filtering

--- prototype/server/routes/api/sports.js
+++ prototype/server/routes/api/sports.js
@@ -5,13 +5,26 @@ const axios = require('axios');
 const SPORTS_API_KEY = process.env.SPORTS_API_KEY;
 const SPORTS_API_URL = process.env.SPORTS_API_URL;
 
+// Leagues:
+// 13014 - UEFA Champions
+// 14431 - CONCACAF Champions
+// 1325 - Portugal 1st League
+// 18446 - USL First Division
+
+var preferedLeagues = new Array("13014","14431","1325","18446","133411");
+
+
 const liveSports = () => new Promise((resolve, reject) => {
     axios.get(`${SPORTS_API_URL}/sport/football/livescores?api_key=${SPORTS_API_KEY}`)
     .then(response => {
-        console.log(response.data)
+        var filt = response.data.data.filter(function(element){
+            return preferedLeagues.includes(element.leagueId);
+        });
 
+        console.log(filt);
         return response.data;
     })
+
 })
 
 router.get('/live', (req, res) => {
