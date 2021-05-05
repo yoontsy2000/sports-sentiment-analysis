c94bbf0d1c9e74aed54d6b93a4b24994c368fafe
Alvaro Carrascosa <varocarras@gmail.com>
2021-05-05 15:42:39 -0400

    Update sports.js

--- prototype/server/routes/api/sports.js
+++ prototype/server/routes/api/sports.js
@@ -6,23 +6,27 @@ const SPORTS_API_KEY = process.env.SPORTS_API_KEY;
 const SPORTS_API_URL = process.env.SPORTS_API_URL;
 
 // Leagues:
-// 13014 - UEFA Champions
-// 14431 - CONCACAF Champions
+// 13014 - UEFA Champions League
+// 14431 - CONCACAF Champions League
 // 1325 - Portugal 1st League
 // 18446 - USL First Division
 
-var preferedLeagues = new Array("13014","14431","1325","18446","133411");
+var preferedLeagues = new Array(
+    "13014","14431","1325","18446","133411");
+
+var activeStatuses = new Array(
+    1,2,3,4,5);
 
 
 const liveSports = () => new Promise((resolve, reject) => {
     axios.get(`${SPORTS_API_URL}/sport/football/livescores?api_key=${SPORTS_API_KEY}`)
     .then(response => {
-        var filt = response.data.data.filter(function(element){
-            return preferedLeagues.includes(element.leagueId);
-        });
 
-        console.log(filt);
-        return response.data;
+        var filteredResponse = response.data.data.filter(function(element){
+            return preferedLeagues.includes(element.leagueId) && activeStatuses.includes(element.status);
+        });
+        console.log(filteredResponse);
+        return filteredResponse;
     })
 
 })
@@ -32,4 +36,5 @@ router.get('/live', (req, res) => {
         res.send(response);
     })
 })
+
 module.exports = router;
