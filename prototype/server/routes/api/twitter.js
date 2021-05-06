7f14f23b82643322add625f35e2a569278368556
yoontsy2000 <yoontsy2000@gmail.com>
2021-05-05 21:10:35 -0400

    Slight formatting fix

--- prototype/server/routes/api/twitter.js
+++ prototype/server/routes/api/twitter.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const Twitter = require('twitter');
 const config = require('dotenv/config');
 
-const API_KEY = process.env.API_KEY;
-const API_SECRET_KEY = process.env.API_SECRET_KEY;
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-const ACESSS_SECRET_TOKEN = process.env.ACESSS_SECRET_TOKEN;
+const TWITTER_API_KEY = process.env.TWITTER_API_KEY;
+const TWITTER_API_SECRET_KEY = process.env.TWITTER_API_SECRET_KEY;
+const TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN;
+const TWITTER_ACESSS_SECRET_TOKEN = process.env.TWITTER_ACESSS_SECRET_TOKEN;
 
 const client = new Twitter({
-  consumer_key: API_KEY,
-  consumer_secret: API_SECRET_KEY,
-  access_token_key: ACCESS_TOKEN,
-  access_token_secret: ACESSS_SECRET_TOKEN
+  consumer_key: TWITTER_API_KEY,
+  consumer_secret: TWITTER_API_SECRET_KEY,
+  access_token_key: TWITTER_ACCESS_TOKEN,
+  access_token_secret: TWITTER_ACESSS_SECRET_TOKEN
 });
 
 //Twitter API for finding the tweets with the tag that the use inputs.
 //gives back the top 5 tweets of that specified tag
+
 const search_tweets = (query) => new Promise((resolve, reject) => {
   client.get('search/tweets', {q: query, result_type: 'popular', count: 5}, function(error, tweets, response) {
     if (!error) {
@@ -33,6 +34,7 @@ router.get('/test', (req, res) => {
 
 //Twitter search bar 
 //It will search whatever is in query into Twitter and return the tweets as results
+
 router.get('/search', (req, res) => {
   const query = req.query.search
   console.log(query)
--- prototype/src/Dashboard.js
+++ prototype/src/Dashboard.js
@@ -47,6 +47,7 @@ export default function Dashboard() {
     }).then(response => {
       console.log(response.data)
       setTones(response.data.tones)
+      tones.forEach()
     })
     .catch(error => console.log(error))
   }
